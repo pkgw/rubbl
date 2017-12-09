58caff5c7bba62726b0f7f26438da4bd3c86d157
Peter Williams <peter@newton.cx>
2017-12-09 16:05:08 -0500

    core: ensure that everything is documented

--- core/src/io.rs
+++ core/src/io.rs
@@ -15,6 +15,14 @@ use std::io::Read;
 use errors::{ErrorKind, Result};
 
 
+/// This struct wraps a Read type to equip it with hooks to track its
+/// alignment — that is, how many bytes into the stream the read has
+/// progressed, and whether the current offset is an exact multiple of a
+/// certain number of bytes from the beginning.
+///
+/// Streams often have alignment requirements so that they can safely be
+/// mapped into in-memory data structures. In particular, this is the case for
+/// MIRIAD files.
 #[derive(Debug)]
 pub struct AligningReader<R: Read> {
     inner: R,
@@ -23,6 +31,7 @@ pub struct AligningReader<R: Read> {
 
 
 impl<R: Read> AligningReader<R> {
+    /// Create a new AligningReader that wraps the argument *inner*.
     pub fn new(inner: R) -> Self {
         AligningReader {
             inner: inner,
@@ -30,17 +39,27 @@ impl<R: Read> AligningReader<R> {
         }
     }
 
-
+    /// Consume this struct, returning the underlying inner reader.
     pub fn into_inner(self) -> R {
         self.inner
     }
 
-
+    /// Return how many bytes we have read since this struct was created.
+    ///
+    /// Note that this offset is tracked internally. If you open a file, raed
+    /// part of it, and *then* create an AligningReader, the returned offset
+    /// will refer to the number of bytes read since creation, not the actual
+    /// file position as understood by the underlying OS.
     pub fn offset(&self) -> u64 {
         self.offset
     }
 
-
+    /// Read and discard bytes to ensure that the stream is aligned as specified.
+    ///
+    /// The maximum allowed alignment value is 64 bytes.
+    ///
+    /// Returns whether the stream was already at the right alignment. When
+    /// that is the case, no read is performed.
     pub fn align_to(&mut self, alignment: usize) -> Result<bool> {
         let mut buf = [0u8; 64];
 
@@ -78,9 +97,28 @@ impl<R: Read> Read for AligningReader<R> {
 }
 
 
+/// This is an extension trait that makes it more convenient to handle errors
+/// when opening files that may be missing.
+///
+/// Various parts of Rubbl try to open files but don’t mind if the files are
+/// missing. This is expressed using return types like `Result<Option<File>>`:
+/// if the value is `Ok(None)`, that means that the file was not found, even
+/// though the underlying I/O operation probably return an `Err` type.
+///
+/// There are times when we want to use these APIs, but it actually is an
+/// error if the file in question is missing. This trait provides a
+/// `require_found` method on the `Result<Option<T>>` type that removes the
+/// `Option` layer of the type, converting `Ok(None)` into an `Err` containing
+/// a `NotFound` error.
 pub trait OpenResultExt {
+    /// The output type of the `require_found` method.
+    ///
+    /// `Result<Option<T>>` becomes `Result<T>`. Due to the way the trait is
+    /// specified, we have to use an associated type to express this fact.
     type Reprocessed;
 
+    /// If *self* is `Ok(None)`, convert it into an `Err` with a `NotFound`
+    /// type.
     fn require_found(self) -> Self::Reprocessed;
 }
 
@@ -103,6 +141,9 @@ impl<T> OpenResultExt for Result<Option<T>> {
 }
 
 
+/// Extend the `Read` trait to provide functions for reading an exact number
+/// of bytes from a stream and distinguishing whether EOF was encountered
+/// immediately, versus whether it was encountered in the midst of the read.
 pub trait EofReadExactExt: Read {
     /// Like `Read::read_exact`, except returns Ok(false) if EOF was
     /// encountered at the first read attempt. Returns Ok(true) if everything
@@ -124,6 +165,9 @@ pub trait EofReadExactExt: Read {
         }
     }
 
+    /// Like `byteorder::ReadBytesExt::read_f32::<BigEndian>`, except returns
+    /// Some(n) on success and None if EOF was encountered at the first read
+    /// attempt.
     fn eof_read_be_f32(&mut self) -> Result<Option<f32>> {
         let mut buf = [0u8; 4];
 
@@ -134,6 +178,10 @@ pub trait EofReadExactExt: Read {
         }
     }
 
+    /// Like `byteorder::ReadBytesExt::read_f32::<BigEndian>`, except it reads
+    /// two values and packs them into a `Complex<f32>`, and returns Some(n)
+    /// on success and None if EOF was encountered at the first read attempt.
+    /// The real part comes before the imaginary part.
     fn eof_read_be_c64(&mut self) -> Result<Option<Complex<f32>>> {
         let mut buf = [0u8; 8];
 
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,6 +1,17 @@
 // Copyright 2017 Peter Williams
 // Licensed under the MIT License.
 
+/*!
+
+Core types and concepts of the Rubbl framework.
+
+This crate provides low-level types that are expected to be used throughout
+the Rubbl framework.
+
+*/
+
+#![deny(missing_docs)]
+
 extern crate byteorder;
 extern crate clap;
 #[macro_use] extern crate error_chain;
