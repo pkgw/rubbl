bef47aa9f1f43e25124c7fa0f889939c276a386b
Peter Williams <peter@newton.cx>
2017-12-20 10:40:06 -0500

    casatables: fix repo layout to enable Cargo publishing

--- casatables/build.rs
+++ casatables/build.rs
@@ -1,6 +1,17 @@
 // Copyright 2017 Peter Williams <peter@newton.cx> and collaborators
 // Licensed under the MIT License.
 
+/*!
+
+In the Git checkout, the `src/` directory contains a symbolic link to
+`../casatables_impl/casacore`. When building from a checkout, this works
+straightforwardly. When creating a package that is uploaded to crates.io,
+Cargo does the right thing and copies over all of the files, which is a bit
+bloaty but means that we can maintain the helpful casatables/casatables_impl
+split.
+
+*/
+
 extern crate gcc;
 
 
@@ -14,7 +25,6 @@ fn main() {
     builder
         .cpp(true)
         .warnings(true)
-        .include("../casatables_impl")
         .include("src")
         .files(FILES)
         .compile("libcasatables_glue.a");
