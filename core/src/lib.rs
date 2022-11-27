dc3f9e544d59b6524b86e51bbd8a0f986cfad1ad
Christopher H. Jordan <christopherjordan87@gmail.com>
2022-11-27 18:13:27 +0800

    Clean up dependencies.
    Make anyhow, clap and termcolor optional dependencies in rubbl_core;
    they are now activated by the "notifications" feature. This means that
    downstream consumers of e.g. rubbl_casacore don't pull these
    dependencies in which aren't actually used in the library.
    
    Remove approx as a dependency. This was only in the hope of reducing
    friction between date-type-exporting crates like num-complex, but, (now
    that we all understand this a bit better,) this doesn't do anything
    here.

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -12,16 +12,7 @@
 pub use ndarray::{Array, CowArray};
 pub use num_complex::Complex;
 
-// `approx` isn't (as of October 2021) used anywhere in Rubbl, but by including
-// it as a dependency, we can get implementations of its traits for the Complex
-// type that we export. Re-exporting the crate gives users an ability to name
-// these traits if so desired.
-//
-// We could make this optional with a Cargo feature, for downstream users that
-// don't need the functionality, but it's a very lightweight dependency, so
-// let's just keep things simple.
-pub use approx;
-
 pub mod io;
+#[cfg(feature = "notifications")]
 pub mod notify;
 pub mod num;
