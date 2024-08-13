fcb6bf3398f90fec1f55c5b0fc262ab9de454509
Peter Williams <peter@newton.cx>
2024-08-13 14:50:51 -0400

    casatables_impl/build.rs: try to silence macOS build warnings
    Change suggested by @d3v-null.

--- casatables_impl/build.rs
+++ casatables_impl/build.rs
@@ -8,6 +8,8 @@ fn main() {
         .cpp(true)
         .warnings(true)
         .flag_if_supported("-std=c++11")
+        // Silences a lot of warnings on macOS
+        .flag_if_supported("-Wno-deprecated-declarations")
         // This allows us to treat rubbl's modified casacore as a separate
         // namespace, so that both vanilla casacore and rubbl can be linked
         // at the same time.
